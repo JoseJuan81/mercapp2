f3405aa03b930f86b52e96600ca9d72f096b8ad4
josejuan81 <pe.dominguezjosejuan@gmail.com>
2022-04-19 19:24:25 -0500

    fetch event para retornar lo que esta en la cache

--- public/sw-fetch.js
+++ public/sw-fetch.js
@@ -1,4 +1,25 @@
 
+const db = {};
+
+// Inicializar indexDB de Compras
+
+const requestPurchase = window.indexedDB.open('MisCompras', 1);
+requestPurchase.onsuccess = function( event ) {
+    db.purchasesDB = event.result;
+}
+requestPurchase.onerror = function( err ) {
+    window.alert("Por que no deja que esta app use indexDB ????");
+}
+
+// Inicializar indexDB de Insumos
+const requestInsumos = window.indexedDB.open('MisInsumos', 1);
+requestInsumos.onsuccess = function( event ) {
+    db.insumosDB = event.result;
+}
+requestInsumos.onerror = function( err ) {
+    window.alert("Por que no deja que esta app use indexDB ????");
+}
+
 // con esto retorno la informacion si esta en la cache, de los contrario consumo el servicio
 self.addEventListener('fetch', function(e){
     console.log('[ServiceWorker] Fetch', e.request)
--- src/sw-register.js
+++ src/sw-register.js
@@ -1,20 +1,14 @@
 export default function LocalServiceWorkerRegister() {
     const swShellPath = `${process.env.PUBLIC_URL}/sw-shell.js`;
-    // const swFetchPath = `${process.env.PUBLIC_URL}/sw-fetch.js`;
-    // if ('serviceWorker' in navigator) {
-    if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'production') {
+
+    if ('serviceWorker' in navigator) {
+    // if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'production') {
 
         window.addEventListener('load', function () {
             navigator.serviceWorker.register(swShellPath).then(registration => {
-                console.log('Service worker registered');
+                console.log('SHELL Service worker :: registered');
             });
         });
-
-        // window.addEventListener('load', function () {
-        //     navigator.serviceWorker.register(swFetchPath).then(registration => {
-        //         console.log('Service worker registered');
-        //     });
-        // });
 
     }
 }
